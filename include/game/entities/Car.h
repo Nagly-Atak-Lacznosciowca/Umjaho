c6ad121bc3d0d0a31cb6bcc603316070f9902c12
Niemiets <84541866+Niemiets@users.noreply.github.com>
2025-03-29 19:37:11 +0100

    Slowing down car

--- include/game/entities/Car.h
+++ include/game/entities/Car.h
@@ -7,38 +7,35 @@
 #include "engine/Renderer.h"
 
 class Car : public SceneElement {
-public:
-	static const double WIDTH;
-	static const double LENGTH;
-    static const double NITRO_MULTIPLIER;
-    static const int NEEDED_CHARGES;
-    static const int NITRO_TIME;
-	
-	const double brakeStrength = 0.1;
-	const double turnGain = 0.001;    // Angle gain per frame
-	
-    double maxSpeed = 6;
+	public:
+		static const double WIDTH;
+		static const double LENGTH;
+	    static const double NITRO_MULTIPLIER;
+	    static const int NEEDED_CHARGES;
+	    static const int NITRO_TIME;
 		
-    double acceleration = 0.05;
-    double speed = 0.0;
-    double turnAngle = 0.0;     // Current angle of the wheels in radians
-	double maxTurnAngle = 0.03; // Max angle of the wheels
-
-
-
-
-	bool canMove = true;
-
-    Car(double x, double y, double width = Car::WIDTH, double length = Car::LENGTH, double angle = 0, double zIndex = 0, SDL_Texture* texture = nullptr);
-    void move();
-    void decelerate();
-    void accelerate();
-    void brake();
-    void reverse();
-    void turnLeft();
-    void turnRight();
-    void straighten();
-	void collide(SceneElement* element) override;
+		const double brakeStrength = 0.05;
+		const double turnGain = 0.001;    // Angle gain per frame
+		
+	    double maxSpeed = 4;
+			
+	    double acceleration = 0.02;
+	    double speed = 0.0;
+	    double turnAngle = 0.0;     // Current angle of the wheels in radians
+		double maxTurnAngle = 0.03; // Max angle of the wheels
+	
+		bool canMove = true;
+	
+	    Car(double x, double y, double width = Car::WIDTH, double length = Car::LENGTH, double angle = 0, double zIndex = 0, SDL_Texture* texture = nullptr);
+	    void move();
+	    void decelerate();
+	    void accelerate();
+	    void brake();
+	    void reverse();
+	    void turnLeft();
+	    void turnRight();
+	    void straighten();
+		void collide(SceneElement* element) override;
 };
 
 #endif //UMJAHO_CAR_H
