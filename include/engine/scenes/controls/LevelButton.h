1b15ddc5b489548ccc2eb1f18df813e58ebbfc36
Niemiets <84541866+Niemiets@users.noreply.github.com>
2025-03-31 13:17:37 +0200

    LevelButton using template

--- include/engine/scenes/controls/LevelButton.h
+++ include/engine/scenes/controls/LevelButton.h
@@ -4,16 +4,17 @@
 #include "Button.h"
 #include "engine/scenes/scenes/Level.h"
 
+template <typename LevelClass>
 class LevelButton: public Button{
 public:
-    LevelButton(double x, double y, double width, double height, double angle, double zIndex, SDL_Texture *texture,
-                Level *level, const std::string &text, SDL_Texture* image);
-
-    Level* level;
+    LevelButton(double x, double y, double width, double height, double angle, double zIndex, SDL_Texture *texture, const std::string &text, SDL_Texture* image);
+	
     SDL_Texture* image;
 
     void click(float x, float y) const override;
     void render() override;
 };
+
+#include "engine/scenes/controls/LevelButton.tpp"
 
 #endif //UMJAHO_LEVELBUTTON_H
