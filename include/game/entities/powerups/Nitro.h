a50428e08494a801974cf64e17b403ba4a68bd3b
Niemiets <84541866+Niemiets@users.noreply.github.com>
2025-03-31 20:28:36 +0200

    Nitro powerup texture

--- include/game/entities/powerups/Nitro.h
+++ include/game/entities/powerups/Nitro.h
@@ -1,10 +1,12 @@
 #ifndef UMJAHO_NITRO_H
 #define UMJAHO_NITRO_H
 #include "game/entities/powerups/PowerUp.h"
+#include "game/Game.h"
+#include <cmath>
 
 class Nitro: public PowerUp{
 public:
-    Nitro(double x, double y, double width = 50, double height = 50, double angle = 0, double zIndex = 0, SDL_Texture* texture = nullptr);
+    Nitro(double x, double y, double width = 55, double height = 20, double angle = M_PI / 4, double zIndex = 0, SDL_Texture* texture = Game::textures.at("nitro3.png"));
     void action(Player* player) override;
 };
 
